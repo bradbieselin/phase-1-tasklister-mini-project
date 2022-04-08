4d0332a7294cb2056b84f015190435e8cf556879
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-08 15:14:25 -0400

    created DOMContentLoaded eventlistener, created todo function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault()
+    buildToDo(e["target"]["new-task-description"]["value"]);
+    form.reset();
+  })
 });
+
+function buildToDo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x";
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  console.log(li);
+  document.getElementById("tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
